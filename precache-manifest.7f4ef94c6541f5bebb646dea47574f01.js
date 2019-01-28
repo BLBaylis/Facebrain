b49bc6e31da912fd04b9fd81162d67d3e7b3a164
BLBaylis <bradleybaylis@hotmail.com>
2019-01-28 20:46:18 +0000

    Updates

---
+++ precache-manifest.7f4ef94c6541f5bebb646dea47574f01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "02658f9a840df64219d0",
+    "url": "/facebrain/static/js/runtime~main.02658f9a.js"
+  },
+  {
+    "revision": "e69a660d18b344c5a7e8",
+    "url": "/facebrain/static/js/main.e69a660d.chunk.js"
+  },
+  {
+    "revision": "1e747438c9cb1f5f7479",
+    "url": "/facebrain/static/js/1.1e747438.chunk.js"
+  },
+  {
+    "revision": "e69a660d18b344c5a7e8",
+    "url": "/facebrain/static/css/main.719687df.chunk.css"
+  },
+  {
+    "revision": "1e747438c9cb1f5f7479",
+    "url": "/facebrain/static/css/1.8e7c208d.chunk.css"
+  },
+  {
+    "revision": "f0c9ae80c31ff8c4037a31db71cc3df5",
+    "url": "/facebrain/index.html"
+  }
+];
